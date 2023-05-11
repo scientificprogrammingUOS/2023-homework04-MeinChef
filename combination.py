fca07c7857b89296b6fc9345fe104174de7c9e2a
Fabian <yapa.nilop@gmail.com>
2023-05-11 18:25:35 +0200

    added addition

--- combination.py
+++ combination.py
@@ -3,10 +3,7 @@ import numpy as np
 # implement your function to combine two numpy arrays 
 
 def combination(array0, array1, axis = 0):
-    # delete the NotImplementedError when you write your function.
     try:
-        #np.dtype(array0);
-        #np.dtype(array1);
         array0 = np.squeeze(array0);
         array1 = np.squeeze(array1);
         print(array0);
@@ -19,12 +16,5 @@ def combination(array0, array1, axis = 0):
     except TypeError:
         print("TypeError: You didn't give a fucking array as input. Change that NOW!")
 
-
-
 if __name__ == "__main__":
-    #a = np.arange(0,4);
-    #b = np.arange(5,7);
-    #print(a);
-    #print(b);
-    #combination(a, b);
     pass
--- gaussian_analysis.py
+++ gaussian_analysis.py
@@ -2,13 +2,23 @@ import numpy as np
 
 # implement the function gaussian_analysis
 
-def gaussian_analysis():
+def gaussian_analysis(loc, scale, lower_bound, upper_bound):
         
-    # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
+    if( isinstance(loc, (int, float)) and isinstance(scale, (int, float)) and isinstance(upper_bound, (int, float)) and isinstance(lower_bound, (int, float)) and lower_bound < upper_bound ):
+        
+        SAMPLE_SIZE = 200;
+        samples = np.random.normal(loc, scale, SAMPLE_SIZE);
+        mask = (samples > lower_bound) & (samples < upper_bound);
+        samples = samples[mask];
 
+        mean = abs(np.sum(samples)/len(samples));    
+        std = np.sqrt(np.sum(np.square(samples - mean)) / len(samples));
+
+        return (mean, std);
+
+    else:    
+        raise TypeError("You either gave that funciont a ");
 
 if __name__ == "__main__":
     # use this for your own testing!
-
-    pass
+    pass    
